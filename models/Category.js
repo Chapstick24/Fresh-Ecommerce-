e1dc8920265f45f4aa9cbb6e22989c92b39b49bb
mychal <mychal9251@gmail.com>
2021-04-22 22:38:22 -0700

    updating category table

--- models/Category.js
+++ models/Category.js
@@ -2,12 +2,23 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
-
+class Category extends Model { }
+// creat a table for caterogy modul, ID and Name will be the columns. 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
+
   {
     sequelize,
     timestamps: false,
